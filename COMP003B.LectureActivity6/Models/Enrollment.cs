05be1af55b523933025cc61dcbe9b6f9c3b9684c
flores-97 <floresvictor15@gmail.com>
2025-04-28 09:35:20 -0700

    created three classes from lecture activity

---
+++ COMP003B.LectureActivity6/Models/Enrollment.cs
@@ -0,0 +1,15 @@
+﻿namespace COMP003B.LectureActivity6.Models
+{
+    public class Enrollment
+    {
+        public int Id { get; set; } 
+        public int StudentId { get; set; }  
+        public int CourseId { get; set; }
+
+        //nullable navigation properties
+        public virtual Student? Student { get; set; }
+        public virtual Course? Course { get; set; }
+
+
+    }
+}
